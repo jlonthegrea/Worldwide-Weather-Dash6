2078161a80c77b708f9135a9d7420f26ef51b9d7
jlonthegrea <allen_jaylon@yahoo.com>
2024-01-18 19:09:17 -0500

    Added to click event handler

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 const APIkey = "2ae0fed7d9cace10869d3b92643028e3";
 var cityName;
 
-
+// Getting LAT&LON
 function getLatLon() {
     cityName = $('#cityName')[0].value;
 
-    const apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=2ae0fed7d9cace10869d3b92643028e3"
+    const apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&units=imperial&appid=2ae0fed7d9cace10869d3b92643028e3"
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -17,26 +17,26 @@ function getLatLon() {
 
 }
 
+// Getting Current Weather
 function getCurrentWeather(lat, lon) {
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIkey}`
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
                 renderCurrentWeather(data);
             })
         }
     })
 }
 
+// Getting Forecast for 5 day
 function getForecast() {
 
-    const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=2ae0fed7d9cace10869d3b92643028e3";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=2ae0fed7d9cace10869d3b92643028e3";
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
                 renderFutureWeather(data);
             })
         }
@@ -45,7 +45,7 @@ function getForecast() {
 
 }
 
-// Current Weather details
+// Render Weather details onto the page 
 function renderCurrentWeather(data) {
 
     $("#city-name").text(cityName).val + dayjs().format("MM/DD/YYYY");
@@ -55,27 +55,36 @@ function renderCurrentWeather(data) {
 }
 
 
-// Future Weather details for 5 day Forecast
+// Render Future Weather details for 5 day Forecast onto the page 
 function renderFutureWeather(data) {
+    console.log(data);
 
+    
 
-    for (var i = 0; i < 5; i++) {
-        var weatherTmr = {
-            date: dayjs().format("MM/DD/YYY"),
-            temp: data.daily[i + 1].temp.day.toFixed(1),
-            wind_speed: data.daily[i + 1].wind_speed.toFixed(1),
-            humidity: data.daily[i + 1].humidity
+    // $("#img-0").src (data.list[0].weather[0]);
+    $("#day-0").text(data.list[0].dt_txt);
+    $("#temp-0").text("Temp: " + data.list[0].main.temp + " Fahrenheit");
+    $("#wind-speed-0").text("Wind Speed: " + data.list[0].wind.speed + " MPH");
+    $("#hum-0").text("Humidity: " + data.list[0].main.humidity + " %");
+    
 
-        }
-        var Selector = "#day-" + i;
-        $(Selector)[0].textContent = weatherTmr.date;
-        Selector = "#temp-" + i
-        $(Selector)[0].textContent = "Temp: " + weatherTmr.temp + "Fahrenheit";
-        Selector = "#wind-speed-" + i
-        $(Selector)[0].textContent = "Wind Speed: " + weatherTmr.wind_speed + "MPH";
-        Selector = "#hum-" + i
-        $(Selector)[0].textContent = "Humidity: " + weatherTmr.humidity + "%";
-    }
+    // for (var i = 0; i < 5; i++) {
+    //     var weatherTmr = {
+    //         date: dayjs().format("MM/DD/YYY"),
+    //         temperature: data.list.main.temp,
+    //         wind_speed: data.wind.speed,
+    //         humidity: data.main.humidity
+
+    //     }
+    //     var Selector = "#day-" + i;
+    //     $(Selector).text (weatherTmr.date);
+    //     Selector = "#temp-" + i
+    //     $(Selector).text ("Temp: " + weatherTmr.temp + " Fahrenheit");
+    //     Selector = "#wind-speed-" + i
+    //     $(Selector).text ("Wind Speed: " + weatherTmr.wind_speed + " MPH");
+    //     Selector = "#hum-" + i
+    //     $(Selector).text ("Humidity: " + weatherTmr.humidity + " %");
+    // }
 }
 
 // Submit button 
@@ -83,5 +92,6 @@ $("#search-button").on("click", function (e) {
     e.preventDefault();
     getLatLon();
     getForecast();
+    $(".forecast-panel").addClass("visible");
 })
 
