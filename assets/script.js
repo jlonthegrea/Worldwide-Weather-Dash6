b7239e2b56b4c753a03671ee5217f499774cb503
jlonthegrea <allen_jaylon@yahoo.com>
2024-01-18 22:43:44 -0500

    Added emblems to 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -34,6 +34,10 @@ function getCurrentWeather(lat, lon) {
 function getForecast() {
 
     const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=2ae0fed7d9cace10869d3b92643028e3";
+
+
+    // const apiUrl = "https://pro.openweathermap.org/data/2.5/forecast/climate?q=" + cityName + "&units=imperial&appid=2ae0fed7d9cace10869d3b92643028e3"
+
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -48,6 +52,8 @@ function getForecast() {
 // Render Weather details onto the page 
 function renderCurrentWeather(data) {
 
+    $()
+
     $("#city-name").text(cityName).val 
     $("#date-now").text (dayjs().format("MM/DD/YYYY"));
     $("#temperature").text("Temp: " + data.main.temp + " Fahrenheit");
@@ -59,9 +65,12 @@ function renderCurrentWeather(data) {
 // Render Future Weather details for 5 day Forecast onto the page 
 function renderFutureWeather(data) {
 console.log(data);
+
+var iconUrl = "https://openweathermap.org/img/w/"
+
     for (var i = 0; i < data.list.length; i++) {
         $(`#day-${i}`).text(data.list[i].dt_txt);
-        // $(`#img-${i}`). (data.list[i].weather[0].icon)
+        $(`#img-${i}`).attr('src', `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`);
         $(`#temp-${i}`).text("Temp: " + data.list[i].main.temp + " Fahrenheit");
         $(`#wind-speed-${i}`).text("Wind Speed: " + data.list[i].wind.speed + " MPH");
         $(`#hum-${i}`).text("Humidity: " + data.list[i].main.humidity + " %");
